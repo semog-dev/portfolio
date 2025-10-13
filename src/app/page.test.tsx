59b2927c41065abdc1f875c716ded41712167c05
Fernando <semogdev.pereira@hotmail.com>
2025-10-13 20:42:26 +0000

    voltando com o default na funcao EmConstrucap

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { EmConstrucao } from "./page";
+import EmConstrucao from "./page";
 
 test("Page_DeveRenderizarPageEmConstrucao", () => {
   render(<EmConstrucao />);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { LuConstruction } from "react-icons/lu";
 
-export function EmConstrucao() {
+export default function EmConstrucao() {
   return (
     <div className="flex items-center justify-center gap-2 h-full text-3xl">
       <h1>Em construção...</h1>
