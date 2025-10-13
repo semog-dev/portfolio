1e67c60c6d78bbc1be5268342176af8717e54db9
Fernando <semogdev.pereira@hotmail.com>
2025-10-13 18:26:27 +0000

    Melhorando a pagina 'em construcao'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { LuConstruction } from "react-icons/lu";
+
 export default function Home() {
-  return <h1>Em construção...</h1>;
+  return (
+    <div className="flex items-center justify-center gap-2 h-full text-3xl">
+      <h1>Em construção...</h1>
+      <LuConstruction />
+    </div>
+  );
 }
