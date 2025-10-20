1f81d2899087902124578b424a33af75c1f285fc
Fernando <semogdev.pereira@hotmail.com>
2025-10-20 01:15:57 +0000

    criando a consulta dos projetos pinados no github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { LuConstruction } from "react-icons/lu";
+import { getPinnedProjects } from "../services/projects/projects.service";
 
 export default function EmConstrucao() {
+  const projects = getPinnedProjects();
+
   return (
     <div className="flex items-center justify-center gap-2 h-full text-3xl">
       <h1>Em construção...</h1>
--- vitest.config.mts
+++ vitest.config.mts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { config } from "dotenv";
+
+config({
+  path: ".env",
+});
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
